f401a7f1b38ba6c5df0f2f635f3636555aa7c4da
Edvinas Aleksejonokas <Edvinas.Aleksejonokas@oxid-esales.com>
2016-10-11 12:00:00 +0300

    Fix fatal crash when no option is present in select box
    ref: 0edb782acf77a1542c5b6b3f532f19db8e85f39d

--- library/oxMinkWrapper.php
+++ library/oxMinkWrapper.php
@@ -544,12 +544,9 @@ class oxMinkWrapper extends oxBaseTestCase
             $value = $this->getMinkSession()->getSelectorsHandler()->xpathLiteral($value);
             $aOptions = $oSelect->findAll('xpath', '//option[@value=' . $value . ']');
         }
-        $oOption = array_pop($aOptions);
+        $oOption = !empty($aOptions) ? array_pop($aOptions) : $oSelect->find('xpath', 'option');
 
-        if (is_null($oOption)) {
-            return $oSelect->find('xpath', 'option')->getText();
-        }
-        return $oOption->getText();
+        return $oOption ? $oOption->getText() : '';
     }
 
     /**
